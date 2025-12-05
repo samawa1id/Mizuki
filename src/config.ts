40ae9584b8ed19d009acb10bc7b2c4ccb2445d00
Sama <165390112+samawa1id@users.noreply.github.com>
2025-12-05 17:46:51 +0200

    Update profile avatar image path

--- src/config.ts
+++ src/config.ts
@@ -275,7 +275,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/avatar.webp", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
+	avatar: "assets/images/pfp.jpg", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
 	name: "0xSky",
 	bio: "Programmer | Pentester | CTF Player",
 	typewriter: {
