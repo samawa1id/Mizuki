fa3c930902027048017788e86f259150d6e97c27
Sama <165390112+samawa1id@users.noreply.github.com>
2025-12-06 15:55:12 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -51,7 +51,8 @@ export const siteConfig: SiteConfig = {
 		// 顶栏标题文本
 		text: "0xSky",
 		// 顶栏标题图标路径，默认使用 public/assets/home/home.png
-		icon: "assets/home/home.png",
+		//icon: "assets/home/home.png",
+		icon: "material-icon:home",
 	},
 
 	bangumi: {
